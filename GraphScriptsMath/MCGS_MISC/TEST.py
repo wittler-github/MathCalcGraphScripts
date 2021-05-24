ad91b429a8da7a76f0dcfb7c348b6714ef681e23
wittler-github <henry.p.a.wittler@gmail.com>
2021-05-24 18:07:19 +0200

    added all even extra files

---
+++ GraphScriptsMath/MCGS_MISC/TEST.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+## Linear algebra
+### Dot product: product of two arrays
+f = np.array([1,2])
+g = np.array([4])
+### 1*4+2*5
+print(np.dot(f, g))
