87527b3d367aad7d7cec4f7d9ac95250de44e574
wittler-github <henry.p.a.wittler@gmail.com>
2021-06-03 18:53:59 +0200

    added KA LinearTransformationExamplesRotationsInR3.py added TEST folder

---
+++ GraphScriptsMath/MCGS_KhanAcademy/LinearTransformationExamplesRotationsInR3.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+
+
+import numpy as np
+import math as m
+
+def Rx(angle):
+  return np.matrix([[ 1, 0           , 0           ],
+                   [ 0, m.cos(angle),-m.sin(angle)],
+                   [ 0, m.sin(angle), m.cos(angle)]])
+
+## Ry, Rz Not defined in KA video
+def Ry(angle):
+  return np.matrix([[ m.cos(angle), 0, m.sin(angle)],
+                   [ 0           , 1, 0           ],
+                   [-m.sin(angle), 0, m.cos(angle)]])
+
+def Rz(angle):
+  return np.matrix([[ m.cos(angle), -m.sin(angle), 0 ],
+                   [ m.sin(angle), m.cos(angle) , 0 ],
+                   [ 0           , 0            , 1 ]])
+
+
+angle_x = m.pi/4
+angle_y = 0
+angle_z = 0
+print("angle_x =", angle_x)
+print("angle_y  =", angle_y)
+print("angle_z =", angle_z)
+
+
+R = Rx(angle_x)
+#* Ry(angle_y) * Rz(angle_z)
+print(np.round(R, decimals=2))
+
+v1 = np.array([[-1],[-3],[4]])
+v2 = R * v1
+print(np.round(v2, decimals=2))
+
+colors=list('bgrcmykw')
+
+fig = plt.figure(figsize=(9,9))
+ax = fig.add_subplot(111, projection='3d')
+
+#ax.scatter(v1[0],v1[1],v1[2],c=colors[2], s=200,alpha=0.25)
+ax.quiver3D(0,0,0,v1[0],v1[1],v1[2],color='g',alpha=1)
+ax.quiver3D(0,0,0,v2[0],v2[1],v2[2],color='m',alpha=1)
+
+#ax.plot(samples[:,0], samples[:,1], samples[:,2], 'o', markersize=10, color='green', alpha=0.2)
+#for v in eig_vec:
+#    ax.plot([mean_x, v[0]], [mean_y, v[1]], [mean_z, v[2]], color='red', alpha=0.8, lw=3)
+ax.set_xlabel('X',color=colors[0])
+ax.set_ylabel('Y',color=colors[3])
+ax.set_zlabel('Z',color=colors[4])
+ax.set_xlim(-5, 5)
+ax.set_ylim(-5, 5)
+ax.set_zlim(-5, 5)
+#ax.axis('equal')
+ax.xaxis.set_major_locator(plt.MultipleLocator(1))
+ax.yaxis.set_major_locator(plt.MultipleLocator(1))
+ax.zaxis.set_major_locator(plt.MultipleLocator(1))
+ax.grid(True)
+
+
+plt.title('Initial and Rotated Vectors')
+
+plt.draw()
+plt.show()
---
+++ GraphScriptsMath/MCGS_KhanAcademy/TEST/TEST1.py
@@ -0,0 +1,23 @@
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Make the grid
+x, y, z = np.meshgrid(np.arange(-10, 0.1, 10),
+                      np.arange(-10, 0.1, 10),
+                      np.arange(-10, 0.1, 10))
+
+# Make the direction data for the arrows
+u = np.sin(np.pi * x) * np.cos(np.pi * y) * np.cos(np.pi * z)
+v = -np.cos(np.pi * x) * np.sin(np.pi * y) * np.cos(np.pi * z)
+w = (np.sqrt(2.0 / 3.0) * np.cos(np.pi * x) * np.cos(np.pi * y) *
+np.sin(np.pi * z))
+
+ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True)
+
+plt.show()
