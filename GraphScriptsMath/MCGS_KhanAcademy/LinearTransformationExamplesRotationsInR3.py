8d160000dfdc9e0a3cc3af9ab975005498cdde9e
wittler-github <henry.p.a.wittler@gmail.com>
2021-06-05 19:54:42 +0200

    Completed LinearTransformationExamplesRotationsInR3.py

--- GraphScriptsMath/MCGS_KhanAcademy/LinearTransformationExamplesRotationsInR3.py
+++ GraphScriptsMath/MCGS_KhanAcademy/LinearTransformationExamplesRotationsInR3.py
@@ -27,19 +27,18 @@ def Rz(angle):
                    [ 0           , 0            , 1 ]])
 
 
-angle_x = m.pi/4
-angle_y = 0
-angle_z = 0
+angle_x = 0#m.pi/2
+angle_y = m.pi/2
+angle_z = 0#m.pi/2
 print("angle_x =", angle_x)
 print("angle_y  =", angle_y)
 print("angle_z =", angle_z)
 
 
-R = Rx(angle_x)
-#* Ry(angle_y) * Rz(angle_z)
+R = Rx(angle_x) * Ry(angle_y) * Rz(angle_z)
 print(np.round(R, decimals=2))
 
-v1 = np.array([[-1],[-3],[4]])
+v1 = np.array([[1],[0],[0]])
 v2 = R * v1
 print(np.round(v2, decimals=2))
 
@@ -49,15 +48,14 @@ fig = plt.figure(figsize=(9,9))
 ax = fig.add_subplot(111, projection='3d')
 
 #ax.scatter(v1[0],v1[1],v1[2],c=colors[2], s=200,alpha=0.25)
-ax.quiver3D(0,0,0,v1[0],v1[1],v1[2],color='g',alpha=1)
-ax.quiver3D(0,0,0,v2[0],v2[1],v2[2],color='m',alpha=1)
+ax.quiver3D(0,0,0,v1[0],v1[1],v1[2],color='g',linewidth=3.3,alpha=1,label='Initial Vector')
+ax.quiver3D(0,0,0,v2[0],v2[1],v2[2],color='m',linewidth=10,alpha=0.25,label='Rotated Vector')
 
-#ax.plot(samples[:,0], samples[:,1], samples[:,2], 'o', markersize=10, color='green', alpha=0.2)
-#for v in eig_vec:
-#    ax.plot([mean_x, v[0]], [mean_y, v[1]], [mean_z, v[2]], color='red', alpha=0.8, lw=3)
-ax.set_xlabel('X',color=colors[0])
-ax.set_ylabel('Y',color=colors[3])
-ax.set_zlabel('Z',color=colors[4])
+ax.legend()
+
+ax.set_xlabel('X',color=colors[0],fontsize=20)
+ax.set_ylabel('Y',color=colors[3],fontsize=20)
+ax.set_zlabel('Z',color=colors[4],fontsize=20)
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 ax.set_zlim(-5, 5)
@@ -68,7 +66,7 @@ ax.zaxis.set_major_locator(plt.MultipleLocator(1))
 ax.grid(True)
 
 
-plt.title('Initial and Rotated Vectors')
+#plt.title('Initial and Rotated Vectors')
 
 plt.draw()
 plt.show()
